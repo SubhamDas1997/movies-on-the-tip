e41e2cd2b626339958043aecd6d4386f064e4088
Subham Das <89313464+SubhamDas1997@users.noreply.github.com>
2022-11-20 12:41:11 +0530

    fix: handled nav btn click style on active

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -13,20 +13,38 @@ interface INavSearch {
 }
 
 const NavMenu = ({searchVal, setSearchVal, disableSearch}: INavSearch) => {
+  const movieType = window.location.pathname;
+
   const [searchOpen, setSearchOpen] = useState<string>('none');
+  const [isFavBtnActive, setIsFavBtnActive] = useState<string>(movieType === "/favourite" ? 'active' : '');
+  const [isSearchBtnActive, setIsSearchBtnActive] = useState<string>('search-btn');
   const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
   const [ExtraSearchOpen, setExtraSearchOpen] = useState<string>(screenWidth <= 1100 ? 'flex' : 'none');
   const [isNavExpanded, setIsNavExpanded] = useState<boolean>(false);
   
-  const handleSeachBtn = () => {
-    if(searchOpen === 'none') setSearchOpen('flex');
-    else setSearchOpen('none');
+  const handleSeachBtnClick = () => {
+    if (searchOpen === 'none') {
+      setSearchOpen('flex');
+      setIsSearchBtnActive('search-btn active');
+    } else {
+      setIsSearchBtnActive('search-btn');
+      setSearchOpen('none');
+    }
+  }
+
+  const handleFavBtnClick = () => {
+    if (isFavBtnActive === '') return setIsFavBtnActive('active');
+  }
+
+  const favBtnInactive = () => {
+    if (isFavBtnActive === 'active') return setIsFavBtnActive('');
   }
 
   const handleHamburgerClick = () => {
     if (screenWidth >= 1100) return setIsNavExpanded(false);
-    if (isNavExpanded === false) setIsNavExpanded(true);
-    else setIsNavExpanded(false);
+    if (isNavExpanded === false) return setIsNavExpanded(true);
+    
+    setIsNavExpanded(false);
   }
 
   useEffect(() => {
@@ -42,7 +60,7 @@ const NavMenu = ({searchVal, setSearchVal, disableSearch}: INavSearch) => {
   return ( 
     <Navbar collapseOnSelect expand="xl" fixed="top" bg="dark" variant="dark" expanded={isNavExpanded}>
       <Container className="nav-container">
-        <Navbar.Brand className="nav-logo" to="/home" as={NavLink} style={{color: '#FFC107'}}>
+        <Navbar.Brand className="nav-logo" to="/home" as={NavLink} style={{color: '#FFC107'}} onClick={favBtnInactive}>
           <FontAwesomeIcon icon={faFilm} style={{marginRight: '5px'}}/> 
           MOTT
         </Navbar.Brand>
@@ -55,10 +73,10 @@ const NavMenu = ({searchVal, setSearchVal, disableSearch}: INavSearch) => {
 
         <Navbar.Collapse id="responsive-navbar-nav" className="no-transition">
           <Nav className="me-auto nav-items" onClick={handleHamburgerClick}>
-            <Nav.Link to="/movies-in-theaters" as={NavLink}>Movies in theaters</Nav.Link>
-            <Nav.Link to="/movies-coming" as={NavLink}>Coming soon</Nav.Link>
-            <Nav.Link to="/top-rated-india" as={NavLink}>Top rated Indian</Nav.Link>
-            <Nav.Link to="/top-rated-movies" as={NavLink}>Top rated movies</Nav.Link>
+            <Nav.Link to="/movies-in-theaters" as={NavLink} onClick={favBtnInactive}>Movies in theaters</Nav.Link>
+            <Nav.Link to="/movies-coming" as={NavLink} onClick={favBtnInactive}>Coming soon</Nav.Link>
+            <Nav.Link to="/top-rated-india" as={NavLink} onClick={favBtnInactive}>Top rated Indian</Nav.Link>
+            <Nav.Link to="/top-rated-movies" as={NavLink} onClick={favBtnInactive}>Top rated movies</Nav.Link>
             <Nav.Link to="/favourite" as={NavLink} className="menu-fav" >Favourties</Nav.Link>
           </Nav>
           
@@ -67,12 +85,12 @@ const NavMenu = ({searchVal, setSearchVal, disableSearch}: INavSearch) => {
               <Search searchVisible={searchOpen} searchVal={searchVal} setSearchVal={setSearchVal} disableSearch={disableSearch} />
             </div>
             
-            <Button className="search-btn" variant="outline-info" style={{margin: "8px 5px 8px 0"}} onClick={handleSeachBtn}>       
+            <Button className={isSearchBtnActive} variant="outline-info" style={{margin: "8px 5px 8px 0"}} onClick={handleSeachBtnClick}>       
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </Button>
 
-            <Nav.Link to="/favourite" as={NavLink} className="btn-fav">          
-              <Button variant="outline-warning">       
+            <Nav.Link to="/favourite" as={NavLink} className="fav-btn-div">          
+              <Button variant="outline-warning" className={isFavBtnActive} onClick={handleFavBtnClick}>       
                 <FontAwesomeIcon icon={faHeart} />
               </Button>
             </Nav.Link>
